d7260d3414087f9da319df2f2290d8764add0efb
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-02-14 15:55:08 -0200

    Add Stylus + Add Heroku config
    Added stylus and a NPM Script to build stylus into css file. Added
    Heroku files and specific settings on package.json and build files.

--- app/config/server.js
+++ app/config/server.js
@@ -1,5 +1,5 @@
 const config = {
-    port: 9000,
+    port: process.env.PORT || 8080,
     host: '127.0.0.1',
     log() {
         console.log(`App was starting!\nListening at ${config.host}:${config.port}...`);
